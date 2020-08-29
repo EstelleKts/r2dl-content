95b967338f9d57ec160e900b311cb1bd87127d67
Estelle NOEL <estelle.noel@gadz.org>
2020-08-29 14:48:42 +0200

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "345c88fb376f8ffc3901";
+/******/ 	var hotCurrentHash = "e402b5313aeb60c67070";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
